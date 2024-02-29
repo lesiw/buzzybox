aa212d31e199d27b4ca1dd36290a7218bea1675a
Christopher Lesiw <chris@chrislesiw.com>
2024-02-29 14:18:47 -0500

    true/false: Update tests to ensure empty output

--- hive/false_test.go
+++ hive/false_test.go
@@ -1,19 +1,38 @@
 package hive_test
 
 import (
+	"strings"
 	"testing"
 
 	"lesiw.io/buzzybox/hive"
 )
 
 func TestFalse(t *testing.T) {
-	if got := hive.Command("false").Run(); got != 1 {
+	cmd := hive.Command("false")
+	cmd.Stdout = &strings.Builder{}
+	cmd.Stderr = &strings.Builder{}
+	if got := hive.Command("false", "--help").Run(); got != 1 {
 		t.Errorf("false returned %d, want 1", got)
+	}
+	if cmd.Stdout.(*strings.Builder).String() != "" {
+		t.Error("stdout not empty")
+	}
+	if cmd.Stderr.(*strings.Builder).String() != "" {
+		t.Error("stderr not empty")
 	}
 }
 
 func TestFalseSwallowsArgv(t *testing.T) {
+	cmd := hive.Command("false", "--help")
+	cmd.Stdout = &strings.Builder{}
+	cmd.Stderr = &strings.Builder{}
 	if got := hive.Command("false", "--help").Run(); got != 1 {
 		t.Errorf("false returned %d, want 1", got)
+	}
+	if cmd.Stdout.(*strings.Builder).String() != "" {
+		t.Error("stdout not empty")
+	}
+	if cmd.Stderr.(*strings.Builder).String() != "" {
+		t.Error("stderr not empty")
 	}
 }
--- hive/true_test.go
+++ hive/true_test.go
@@ -1,19 +1,38 @@
 package hive_test
 
 import (
+	"strings"
 	"testing"
 
 	"lesiw.io/buzzybox/hive"
 )
 
 func TestTrue(t *testing.T) {
-	if got := hive.Command("true").Run(); got != 0 {
+	cmd := hive.Command("true")
+	cmd.Stdout = &strings.Builder{}
+	cmd.Stderr = &strings.Builder{}
+	if got := hive.Command("true", "--help").Run(); got != 0 {
 		t.Errorf("true returned %d, want 0", got)
+	}
+	if cmd.Stdout.(*strings.Builder).String() != "" {
+		t.Error("stdout not empty")
+	}
+	if cmd.Stderr.(*strings.Builder).String() != "" {
+		t.Error("stderr not empty")
 	}
 }
 
 func TestTrueSwallowsArgv(t *testing.T) {
+	cmd := hive.Command("true", "--help")
+	cmd.Stdout = &strings.Builder{}
+	cmd.Stderr = &strings.Builder{}
 	if got := hive.Command("true", "--help").Run(); got != 0 {
 		t.Errorf("true returned %d, want 0", got)
+	}
+	if cmd.Stdout.(*strings.Builder).String() != "" {
+		t.Error("stdout not empty")
+	}
+	if cmd.Stderr.(*strings.Builder).String() != "" {
+		t.Error("stderr not empty")
 	}
 }
